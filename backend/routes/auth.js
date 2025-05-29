cd07bf82eb98e5168cdf9fcf934a7e1c5eac7f9d
Junior <andresvrg007@gmail.com>
2025-05-28 21:36:15 -0600

    feat: ExpenseTracker - Flutter App backend completed

---
+++ backend/routes/auth.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { registerUser } from '../controllers/registerController.js';
+import { loginUser } from '../controllers/loginController.js';
+import { verifyAuth, logout } from '../controllers/authController.js';
+import { updatePassword } from '../controllers/updatePassword.js';
+import { saveSalary, getProfile } from '../controllers/userController.js';
+import { addTransaction, getTransactionsSummary, resetMonthlyTransactions, getTransactions, updateTransaction, deleteTransaction } from '../controllers/transactionController.js';
+
+// Solo define el router y las rutas
+const router = Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/verify', verifyAuth);
+router.post('/logout', logout);
+router.post('/password', updatePassword);
+router.post('/salary', saveSalary);
+router.get('/profile', getProfile);
+router.post('/transaction', addTransaction);
+router.get('/transactions', getTransactions);
+router.put('/transaction/:id', updateTransaction);
+router.delete('/transaction/:id', deleteTransaction);
+router.get('/transactions-summary', getTransactionsSummary);
+router.post('/reset-monthly', resetMonthlyTransactions);
+
+export default router;
+
